a2a89173f2aedd03df3671c46319834f86379bce
mohitcofficial <mohitchandraofficial@gmail.com>
2025-01-03 14:15:36 +0530

    removed reference id from payment recieved page

--- src/app/paymentsuccess/page.js
+++ src/app/paymentsuccess/page.js
@@ -1,17 +1,15 @@
-"use client";
-
-import React from "react";
+// import React, { Suspense } from "react";
 import classes from "./page.module.css";
 import CustomLayout from "@/components/CustomLayout";
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Link from "next/link";
 
 const Page = () => {
-  const searchParams = useSearchParams();
+  // const searchParams = useSearchParams();
 
-  const referenceId = searchParams.get("reference");
+  // const referenceId = searchParams.get("reference");
   const fontSize = { lg: 150, md: 150, sm: 120, xs: 100 };
   const fontSize2 = { lg: 20, md: 18, sm: 16, xs: 14 };
 
@@ -24,9 +22,9 @@ const Page = () => {
           />
           <h1>Payment Received</h1>
           <p>Thank you for your payment!</p>
-          <p>
-            Your reference ID: <strong>{referenceId}</strong>
-          </p>
+          {/* <p>
+              Your reference ID: <strong>{referenceId}</strong>
+            </p> */}
           <p>If you have any questions, please contact our support team.</p>
           <Link href="/" style={{ textDecoration: "none" }}>
             <button className={classes.goBackButton}>
